8c542f12d79ae05ddf43a83b577b96834f348062
zouxu-Home <zouxu9203@163.com>
2019-05-07 21:03:40 +0800

    163邮件配置

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -125,3 +125,13 @@ TINYMCE_DEFAULT_CONFIG = {
     'height': 400,
 }
 
+# 发送邮件配置
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 25
+# 发送邮件的邮箱
+EMAIL_HOST_USER = 'zouxu9203@163.com'
+# 在邮箱中设置的客户端授权密码
+EMAIL_HOST_PASSWORD = 'ttsx123'
+# 收件人看到的发件人
+EMAIL_FROM = '天天生鲜<zouxu9203@163.com>'
