093a3e9304b9d80034b3f5ba2539782d7eccaea5
zouxu9203 <zouxu9203@163.com>
2019-05-08 12:19:23 +0800

    登录、配置redis-session储存，记住账号

--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse # 反向解析
+from django.contrib.auth import authenticate, login
 from django.views.generic import View
 from django.http import HttpResponse
 from django.conf import settings
@@ -171,4 +172,52 @@ class LoginView(View):
 	'''登录'''
 	def get(self, request):
 		'''显示登录页面'''
-		return render(request, 'login.html')
+		# 判断是否记住了用户名
+		if 'username' in request.COOKIES:
+			username = request.COOKIES.get('username')
+			checked = 'checked'
+		else:
+			username = ''
+			checked = ''
+
+		# 使用模板
+		return render(request, 'login.html', {'username':username, 'checked':checked})
+
+	def post(self, request):
+		'''登录校验'''
+		# 接受数据
+		username = request.POST.get('username')
+		password = request.POST.get('pwd')
+
+		# 效验数据
+		if not all([username, password]):
+			return render(request, 'login.html', {'errmsg': '请将数据填写完整'})
+
+		# 业务处理：登录校验
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			# the password verified for the user
+			if user.is_active:
+				# 用户已激活
+				# 记录用户的登录状态
+				login(request, user)
+
+				# 跳转到首页
+				response = redirect(reverse('goods:index'))
+
+				# 判断是否记录用户名
+				remember = request.POST.get('remember')
+				if remember == 'on':
+					# 记住用户名
+					response.set_cookie('username', username, max_age=7*24*3600)
+				else:
+					response.delete_cookie('username')
+
+				# 返回response
+				return response
+			else:
+				# 用户未激活
+				return render(request, 'login.html', {'errmsg': '请先激活用户'})
+		else:
+			# 用户名或密码错误
+			return render(request, 'login.html',  {'errmsg': '用户名或密码错误'})
--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -135,3 +135,17 @@ EMAIL_HOST_USER = 'zouxu9203@163.com'
 EMAIL_HOST_PASSWORD = 'ttsx123'
 # 收件人看到的发件人
 EMAIL_FROM = '天天生鲜<zouxu9203@163.com>'
+
+# Django的缓存配置
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/9",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+# 配置session存储
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
