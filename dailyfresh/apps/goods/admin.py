cfd6bc83bbb30ed970f403bb33621a3e7a2543de
zouxu-Home <zouxu9203@163.com>
2019-05-26 14:26:27 +0800

    首页商品显示内容

--- dailyfresh/apps/goods/admin.py
+++ dailyfresh/apps/goods/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType,Goods,GoodsSKU,IndexGoodsBanner,IndexTypeGoodsBanner,IndexPromotionBanner,GoodsImage
 # Register your models here.
 
 admin.site.register(GoodsType)
+admin.site.register(Goods)
+admin.site.register(GoodsSKU)
+admin.site.register(IndexPromotionBanner)
+admin.site.register(IndexGoodsBanner)
+admin.site.register(IndexTypeGoodsBanner)
+admin.site.register(GoodsImage)
--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner
 # Create your views here.
-def index(request):
-	'''首页内容'''
-	return render(request, 'index.html')
+
+class IndexView(View):
+	def get(self,request):
+		'''首页内容'''
+		# 获取商品的种类信息
+		types = GoodsType.objects.all()
+
+		# 获取首页banner轮播
+		goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+		# 获取首页活动信息
+		promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+		# 获取首页分类商品展示信息
+		for type in types:
+			# 获取type种类首页分类商品的图片展示信息
+			image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+			# 获取type种类首页分类商品的文字展示信息
+			title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+
+			# 动态给type增加属性，分别保存首页分类商品的图片展示信息和文字展示信息
+			type.image_banners = image_banners
+			type.title_banners = title_banners
+
+
+		# 获取用户购物车中商品的数目
+		cart_count = 0
+
+		# 组织模板上下文
+		context = {'types':types,
+				   'goods_banners':goods_banners,
+				   'promotion_banners':promotion_banners,
+				   'cart_count':cart_count
+		}
+
+		return render(request, 'index.html', context)
