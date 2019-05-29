70948a5dc16831d9eb892a6a74c3d0413a6d6831
zouxu9203 <zouxu9203@163.com>
2019-05-29 15:03:39 +0800

    首页模板自动生成,后台修改相应数据后，重新父类保存修改模板增加动态发生任务给celery生成首页模板数据

--- dailyfresh/apps/goods/admin.py
+++ dailyfresh/apps/goods/admin.py
@@ -2,10 +2,48 @@ from django.contrib import admin
 from goods.models import GoodsType,Goods,GoodsSKU,IndexGoodsBanner,IndexTypeGoodsBanner,IndexPromotionBanner,GoodsImage
 # Register your models here.
 
-admin.site.register(GoodsType)
-admin.site.register(Goods)
-admin.site.register(GoodsSKU)
-admin.site.register(IndexPromotionBanner)
-admin.site.register(IndexGoodsBanner)
-admin.site.register(IndexTypeGoodsBanner)
-admin.site.register(GoodsImage)
+'''抽出一个父类进行重写新增、修改、删除数据后，添加任务到celery里面生成新的模板'''
+class BaseModelAdmin(admin.ModelAdmin):
+	def save_model(self, request, obj, form, change):
+		'''新增或更新表中的数据时调用'''
+		super().save_model(request,obj,form,change)
+
+		# 发出任务，让celery worker 重新生成静态页面
+		from celery_tasks.tasks import generate_static_index_html
+		generate_static_index_html.delay()
+
+	def delete_model(self, request, obj):
+		'''删除表中的数据是调用'''
+		super().delete_model(request,obj)
+		# 发出任务，让celery worker 重新生成静态页面
+		from celery_tasks.tasks import generate_static_index_html
+		generate_static_index_html.delay()
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+	pass
+
+class GoodsTypeAdmin(BaseModelAdmin):
+	pass
+
+class GoodsAdmin(BaseModelAdmin):
+	pass
+
+class GoodsSKUAdmin(BaseModelAdmin):
+	pass
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+	pass
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+	pass
+
+class GoodsImageAdmin(BaseModelAdmin):
+	pass
+
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(Goods, GoodsAdmin)
+admin.site.register(GoodsSKU, GoodsSKUAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(GoodsImage, GoodsImageAdmin)
