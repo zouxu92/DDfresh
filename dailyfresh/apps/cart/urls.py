de73eb081f9971703d23b1da070b01a26b9d32e7
zouxu-Home <zouxu9203@163.com>
2019-06-18 00:29:03 +0800

    后台购物车记录更新视图

--- dailyfresh/apps/cart/urls.py
+++ dailyfresh/apps/cart/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from cart.views import CartAddView, CartInfoView
+from cart.views import CartAddView, CartInfoView , CartUpdateView
 
 urlpatterns = [
     url(r'^add$', CartAddView.as_view(), name='add'), # 购物车添加
     url(r'^$', CartInfoView.as_view(), name='show'), # 显示购物车页面
+    url(r'^update$', CartUpdateView.as_view(), name='update'), # 更新购物车记录
 ]
